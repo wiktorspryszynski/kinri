458439c26cce16a6954d13b073fe113347778c9c
wiktorspryszynski <spryszynskiwiktor@gmail.com>
2026-05-29 16:10:11 +0200

    add and edit tasks

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -17,7 +21,51 @@ final class TaskController extends AbstractController
         ]);
     }
 
-    #[Route('/tasks/{id}', name: 'task_details')]
+    #[Route('/tasks/new', name: 'task_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_index');
+        }
+
+        return $this->render('task/form.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/tasks/{id}/edit', name: 'task_edit', requirements: ['id' => '\d+'])]
+    public function edit(int $id, Request $request, TaskRepository $taskRepository, EntityManagerInterface $entityManager): Response
+    {
+        $task = $taskRepository->find($id);
+
+        if ($task === null) {
+            throw $this->createNotFoundException('Task not found.');
+        }
+
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_details', ['id' => $task->getId()]);
+        }
+
+        return $this->render('task/form.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/tasks/{id}', name: 'task_details', requirements: ['id' => '\d+'])]
     public function details(int $id, TaskRepository $taskRepository): Response
     {
         $task = $taskRepository->find($id);
---
+++ src/Form/TaskType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('done')
+            ->add('assignee', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'name',
+                'placeholder' => 'Unassigned',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
